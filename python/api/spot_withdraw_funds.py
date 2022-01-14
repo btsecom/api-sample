ac2fe15091056914ace68cb48e124b668c7f22c2
Yuan-Chen Ho <yuan-chen.ho@nogle.com>
2022-01-14 13:14:11 +0800

    feat: add python wallet withdraw sample

---
+++ python/api/spot_withdraw_funds.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+from requests.exceptions import HTTPError
+from utils import (
+    get_env_info,
+    get_spot_api_version,
+    get_spot_full_url,
+    gen_headers,
+)
+
+
+def spot_withdraw_funds(data):
+    url = "/api/{0}/user/wallet/withdraw".format(get_spot_api_version())
+    env = get_env_info()
+    headers = gen_headers(
+        env["API_KEY"], env["API_SECRET_KEY"], url, json.dumps(data)
+    )
+    ret = {}
+    try:
+        resp = requests.post(
+            get_spot_full_url(env["API_HOST"], url), json=data, headers=headers
+        )
+        resp.raise_for_status()
+    except HTTPError as http_err:
+        print("HTTP error occurred: {0}".format(http_err))
+    except Exception as err:
+        print("Other error occurred: {0}".format(err))
+    else:
+        ret = resp.json()
+    return ret
+
+
+if __name__ == "__main__":
+    print(
+        spot_withdraw_funds(
+            {
+                "currency": "ETH-Ethereum",
+                "address": "0x7293D3B0f486BBB8160fE04f6Fdde0eaf147d6B6",
+                "tag": "",
+                "amount": "0.1",
+            }
+        )
+    )
