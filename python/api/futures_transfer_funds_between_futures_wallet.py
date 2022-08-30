5311acb5ff79e6adafa9d26bb1ca315424f1858c
shao-yu.lee <shao-yu.lee@nogle.com>
2022-08-30 18:16:59 +0800

    add transfer funds POST

---
+++ python/api/futures_transfer_funds_between_futures_wallet.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+from requests.exceptions import HTTPError
+from utils import (
+    get_env_info,
+    get_futures_api_version,
+    get_futures_full_url,
+    gen_headers,
+)
+
+
+def funct(data):
+    url = "/api/{0}/wallet/transfer".format(get_futures_api_version())
+    env = get_env_info()
+    headers = gen_headers(
+        env["API_KEY"], env["API_SECRET_KEY"], url, json.dumps(data)
+    )
+    ret = {}
+    try:
+        resp = requests.post(
+            get_futures_full_url(env["API_HOST"], url),
+            json=data,
+            headers=headers,
+        )
+        resp.raise_for_status()
+    except HTTPError as http_err:
+        print("HTTP error occurred: {0}".format(http_err))
+    except Exception as err:
+        print("Other error occurred: {0}".format(err))
+    else:
+        ret = resp.json()
+    return ret
+
+
+if __name__ == "__main__":
+    data = {
+        "walletSrc": "string",
+        "walletSrcType": "SPOT",
+        "walletDest": "string",
+        "walletDestType": "CROSS",
+        "apiWallets": [
+            {
+            "currency": "USD",
+            "allBalance": "true"
+            },
+            {
+            "currency": "BTC",
+            "allBalance": "true"
+            }
+        ]
+    }
+    print(funct(data))
