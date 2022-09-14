7adbafef78cc13f78ef6b152f57050155563cf1d
Kinvil Hsiao <kinvil.hsiao@nogle.com>
2022-09-14 12:54:33 +0800

    feat: add oco description and selle example

--- python/api/futures_place_oco_order.py
+++ python/api/futures_place_oco_order.py
@@ -35,21 +35,62 @@ def futures_place_oco_order(data):
 
 
 if __name__ == "__main__":
+    """
+    BUY / Long
+    The current BTCPFC price is 20200, I expect its price will downs less 20100 first and turn to ups over than 20400,
+    so I set a "price" to be 20100, and I also set a "triggerPrice" to be 20400 and "stopPrice" to be 20401 at the same time.
+
+    It will create 2 orders at the same time.
+    The order will be completed when any of the below situations occur,
+    and when any of the orders are completed, another order will be canceled.
+    1. when the price is 20100.
+    2. when the price ups directly without downs, it will trigger at price 20400 and attempt to buy at price 20401.
+    """
     print(
         futures_place_oco_order(
             {
                 "clOrderID": "test-order-placement",
                 "size": 1,
-                "price": "20200",
+                "price": "20100",
                 "side": "BUY",
-                "symbol": "BTCPFC",
                 "time_in_force": "GTC",
+                "symbol": "BTCPFC",
+                "triggerPrice": "20400",
+                "stopPrice": "20401",
                 "type": "OCO",
                 "postOnly": false,
                 "reduceOnly": false,
-                "stopPrice": "20401",
-                "triggerPrice": "20400",
                 "trigger": "markPrice",
             }
         )
     )
+    """
+    SELL / Short
+    The current BTCPFC price is 20200, I expect its price will ups over 20400 first and turn to downs less than 20000,
+    so I set a "price" to be 20400, and I also set a "triggerPrice" to be 20101 and "stopPrice" to be 20100 at the same time.
+
+    It will create 2 orders at the same time.
+    The order will be completed when any of the below situations occur,
+    and when any of the orders are completed, another order will be canceled.
+    1. when the price is 20400.
+    2. when the price downs directly without ups, it will trigger at price 20101 and attempt to sell at price 20100.
+    """
+    print(
+        futures_place_oco_order(
+            {
+                "clOrderID": "test-order-placement",
+                "size": 1,
+                "price": "20400",
+                "side": "SELL",
+                "time_in_force": "GTC",
+                "symbol": "BTCPFC",
+                "triggerPrice": "20101",
+                "stopPrice": "20100",
+                "type": "OCO",
+                "postOnly": false,
+                "reduceOnly": false,
+                "trigger": "markPrice",
+            }
+        )
+    )
+    
