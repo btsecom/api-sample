b9c41dae78c252f280a2acfc657b11b3cddbb38f
JerryLi <jerry.li@nogle.com>
2023-10-02 15:10:30 +0800

    Add future make order project

---
+++ CSharp/BESEApiSample/FutureMakeOrderProject/FuturesMakeOrder.cs
@@ -0,0 +1,60 @@
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using BESEApiSample;
+using BESEApiSample.Common;
+
+namespace FutureMakeOrderProject;
+
+public class FuturesMakeOrder
+{
+    private readonly HttpClient _httpClient = new();
+
+    public async Task<List<FutureMakeOrderResponseEntity>?> PlaceOrderAsync(FutureMakeOrderRequestEntity requestEntity)
+    {
+        var requestMessage = GetRequestMessage(requestEntity);
+        try
+        {
+            var response = await _httpClient.SendAsync(requestMessage);
+
+            // If the response is not successful, print the error message
+            if (response.IsSuccessStatusCode is false)
+            {
+                Console.WriteLine($"Error message: {await response.Content.ReadAsStringAsync()}");
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<List<FutureMakeOrderResponseEntity>>();
+        }
+        catch (HttpRequestException httpErr)
+        {
+            Console.WriteLine($"HTTP error occurred: {httpErr.Message}");
+            throw;
+        }
+        catch (Exception err)
+        {
+            Console.WriteLine($"Other error occurred: {err.Message}");
+            throw;
+        }
+    }
+
+    private static HttpRequestMessage GetRequestMessage(FutureMakeOrderRequestEntity requestEntity)
+    {
+        const string endPoint = $"/api/{ApiConstants.FuturesApiVersion}/order";
+        var requestBody = JsonSerializer.Serialize(requestEntity);
+        var request = new HttpRequestMessage(HttpMethod.Post, Utils.GetFuturesFullUrl(endPoint))
+        {
+            Content = new StringContent(requestBody, Encoding.UTF8, "application/json"),
+        };
+
+        // Set headers
+        var headers = Utils.GetHeaders(endPoint, requestBody);
+        foreach (var header in headers)
+        {
+            request.Headers.Add(header.Key, header.Value);
+        }
+
+        return request;
+    }
+}
---
+++ CSharp/BESEApiSample/FutureMakeOrderProject/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using FutureMakeOrderProject;
+
+var futuresOrderService = new FuturesMakeOrder();
+var futurePlaceOrderResponseEntities = await futuresOrderService.PlaceOrderAsync(new FutureMakeOrderRequestEntity()
+{
+    Symbol = "BTCPFC",
+    Price = 21000,
+    Size = 1,
+    Side = "BUY",
+    Type = "LIMIT",
+});
+
+Console.WriteLine($"Response: {JsonSerializer.Serialize(futurePlaceOrderResponseEntities)}");
